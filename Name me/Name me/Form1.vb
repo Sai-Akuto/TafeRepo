eb4d91312e6accdb55c94c97eb855685a4dd97bc
Sai-Akuto <jack.kitchener@gmail.com>
2014-03-27 11:37:48 +0800

    added my name me app

---
+++ Name me/Name me/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Sai-Akuto")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i = 0 To 1000
+            ListBox1.Items.Add("Sai-Akuto")
+        Next i
+    End Sub
+End Class
