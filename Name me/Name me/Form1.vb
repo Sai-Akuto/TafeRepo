5c9f1e4941625037d98ac8f85b2c55ce451f37fc
TheTastyCow <040916399@MY.CENTRAL.WA.EDU.AU>
2014-03-27 12:10:11 +0800

    Thomas Kramer is Cool

--- Name me/Name me/Form1.vb
+++ Name me/Name me/Form1.vb
@@ -1,14 +1,16 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Thomas Kramer")
         ListBox1.Items.Add("Sai-Akuto")
         ListBox1.Items.Add("immabmo, Josh")
         ListBox1.Items.Add("Alan")
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         For i = 0 To 1000
-            ListBox1.Items.Add("Sai-Akuto")
+            ListBox1.Items.Add("Thomas Kramer is Cool")
         Next i
     End Sub
 
